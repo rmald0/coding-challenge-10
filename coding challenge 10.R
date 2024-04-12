4f5bc969e815952a73f4c51394879d5e673383b4
rmald0 <maldrd2@farmingdale.edu>
2024-04-11 20:26:01 -0400

    coding challenge 10

--- coding challenge 10.R
+++ coding challenge 10.R
@@ -1,1 +1,16 @@
-#ayo
+library(wooldridge)
+
+data('hprice1')
+model_formula <- lm(price ~ bdrms, data = hprice1)
+model_fit <- lm(model_formula)
+summary(model_fit)
+
+library(stargazer)
+stargazer(model_fit, type = "text")  
+
+#the the bdrms coefficient is 62.025 therefore adding 1 extra bedroom will add about $62k to the selling price
+
+
+
+
+
